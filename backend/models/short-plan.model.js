cd91308032c4e148ae293e97b75f8d8d7114f92b
thetradecoder <thetradecoder@gmail.com>
2020-09-10 09:56:56 +0600

    local update

---
+++ backend/models/short-plan.model.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const shortPlanSchema = new Schema({
+
+})
+
+module.export = shortPlanSchema;
+
--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-// const mongo_uri = process.env.MONGO_URI;
-require('dotenv');
+require('dotenv').config();
+
+
 
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect()
+const mongoUri = process.env.MONGO_URI;
+mongoose.connect(mongoUri, {useNewUrlParser:true, useUnifiedTopology:true, useCreateIndex:true});
+mongoose.connection.once('open', ()=>{
+    console.log('- mongo connected');
+})
 
 
 
 app.listen(port, ()=>{
-    console.log(`Server is listening on port ${port}`);
+    console.log(`- Server is listening on port ${port}`);
 })
