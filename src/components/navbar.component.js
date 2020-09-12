c6a735e7117c2fc871d787dd44383ea637bdd072
thetradecoder <thetradecoder@gmail.com>
2020-09-13 00:06:12 +0600

    local update

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -7,15 +7,20 @@ import 'jquery/dist/jquery.min.js';
 
 export default function Navbar (){
     return (
-        <nav className="navbar navbar-dark bg-dark">
+        <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
             <div className="container-fluid">
                 <div className="navbar-header">
                     <a href="/" className="navbar-brand">tradecoder</a>
                 </div>
-                <ul className="nav navbar-nav">
-                    <li className="nav-item"><Link to = "/shortplan">Short Term Plan</Link></li>
-                    <li className="nav-item"><Link to ="/longplan">Long Term Plan</Link></li>
-                </ul>
+                <button className="navbar-toggler" data-toggle="collapse" data-target="#main-menu">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div id="main-menu" className="collapse navbar-collapse">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item"><Link to = "/shortplan" className="nav-link">ST Plan</Link></li>
+                        <li className="nav-item"><Link to ="/longplan" className="nav-link">LT Plan</Link></li>
+                    </ul>
+                </div>
             </div>
 
         </nav>
