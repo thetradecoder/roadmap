57b1a6b9e442cb466ac0e3f33726a08d9787d01f
thetradecoder <thetradecoder@gmail.com>
2020-09-13 14:24:58 +0600

    local update

--- src/components/addplans.component.js
+++ src/components/addplans.component.js
@@ -9,14 +9,29 @@ export default function AddPlans(){
         <div>
             <div className="container">
                 <form>
-                    <div className="form-group">
-                        <label>Username:</label>
-                        <input type="text" className="form-control"/>
+                <div className="d-flex flex-wrap justify-content-between">
+                        <div className="form-group">
+                            <label>Username:</label>
+                            <input type="text" className="form-control"/>
+                        </div>
+                        <div className="form-group">
+                            <label>Plan type:</label>
+                            <input type="text" className="form-control"/>
+                        </div>
+                        <div className="form-group">
+                            <label>Start date:</label>
+                            <input type="text" className="form-control"/>
+                        </div>
+                        <div className="form-group">
+                            <label>Deadline:</label>
+                            <input type="text" className="form-control"/>
+                        </div>
+                        <div className="form-group">
+                            <label>Open:</label>
+                            <input type="text" className="form-control"/>
+                        </div>
                     </div>
-                    <div className="form-group">
-                        <label>Plan type:</label>
-                        <input type="text" className="form-control"/>
-                    </div>
+                    
                     <div className="form-group">
                         <label>Plan title:</label>
                         <input type="text" className="form-control"/>
@@ -29,18 +44,8 @@ export default function AddPlans(){
                         <label>Progress:</label>
                         <input type="text" className="form-control"/>
                     </div>
-                    <div className="form-group">
-                        <label>Start date:</label>
-                        <input type="text" className="form-control"/>
-                    </div>
-                    <div className="form-group">
-                        <label>Deadline:</label>
-                        <input type="text" className="form-control"/>
-                    </div>
-                    <div className="form-group">
-                        <label>Open:</label>
-                        <input type="text" className="form-control"/>
-                    </div>
+                  
+                    <button type="submit" className="btn btn-primary">Add plan</button>
                 </form>
             </div>
         </div>
