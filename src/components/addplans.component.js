31c622aa7f67479b7ff95bf80851c525dfbe4c21
thetradecoder <thetradecoder@gmail.com>
2020-09-13 14:41:12 +0600

    local update

--- src/components/addplans.component.js
+++ src/components/addplans.component.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
 export default function AddPlans(){
+
+    const [username, setUsername] =  useState("mamun");
+    const [plantype, setPlantype] =  useState("");
+    const [startdate, setStartdate]= useState(new Date().toLocaleDateString());
+    const [deadline, setDeadline] = useState(new Date().toLocaleDateString());
+    const [open, setOpen] = useState(true);
+    function onChangeUsername(e){
+        setUsername(e.target.value);
+    };
+    function onChangePlantype(e){
+        setPlantype(e.target.value);
+    };
+
+    function onChangeStartdate(e){
+        setStartdate(e.target.value);
+    };
+    function onChangeDeadline(e){
+        setDeadline(e.target.value);
+    }
+    function onChangeOpen(e){
+        setOpen(e.target.value)
+    }
 
     return (
         <div>
@@ -12,23 +34,23 @@ export default function AddPlans(){
                 <div className="d-flex flex-wrap justify-content-between">
                         <div className="form-group">
                             <label>Username:</label>
-                            <input type="text" className="form-control"/>
+                            <input type="text" className="form-control" value={username} onChange={onChangeUsername} required/>
                         </div>
                         <div className="form-group">
                             <label>Plan type:</label>
-                            <input type="text" className="form-control"/>
+                            <input type="text" className="form-control" value={plantype} onChange={onChangePlantype} required/>
                         </div>
                         <div className="form-group">
                             <label>Start date:</label>
-                            <input type="text" className="form-control"/>
+                            <input type="text" className="form-control" value={startdate} onChange={onChangeStartdate} required/>
                         </div>
                         <div className="form-group">
                             <label>Deadline:</label>
-                            <input type="text" className="form-control"/>
+                            <input type="text" className="form-control" value={deadline} onChange={onChangeDeadline} required/>
                         </div>
                         <div className="form-group">
                             <label>Open:</label>
-                            <input type="text" className="form-control"/>
+                            <input type="text" className="form-control" value={open} onCange={onChangeOpen} required/>
                         </div>
                     </div>
                     
